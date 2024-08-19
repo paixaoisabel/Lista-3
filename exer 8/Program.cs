e233ce8249468f98892f9bf68494e6f1b57646a2
paixaoisabel <167146259+paixaoisabel@users.noreply.github.com>
2024-08-19 20:46:07 -0300

    Add files via upload

---
+++ exer 8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exer_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            for (a = 1; a <= 100; a++)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ exer 9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exer_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 1;
+
+            Console.WriteLine("Os trinta primeiros valores da série de Fibonacci são:");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for (int i = 3; i <= 30; i++)
+            {
+                int proximo = a + b;
+                Console.WriteLine(proximo);
+                a = b;
+                b = proximo;
+            }
+        }
+    }
+}
